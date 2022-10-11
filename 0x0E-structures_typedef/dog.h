5e4cde42e21a407305be4e7fd05028d39a71777d
Godwin Adah <adahgodwin224@gmail.com>
2022-10-11 10:43:17 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,6 @@
  * @owner: Third member
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -19,7 +18,6 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
